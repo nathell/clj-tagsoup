32a530318d17413bde13f7fcdaba4e83fb59833d
Daniel Janus <dj@danieljanus.pl>
2010-08-23 16:35:56 +0200

    Release 0.1.2.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-tagsoup "0.1.1"
+(defproject clj-tagsoup "0.1.2"
   :description "A HTML parser for Clojure."
   :repositories {"jn" "http://download.java.net/maven/2"}
   :dependencies [[org.clojure/clojure "1.1.0"]
