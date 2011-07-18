a82c9674d9d178796e36ec58af4c74fc7b53e774
Daniel Janus <dj@danieljanus.pl>
2011-07-18 08:57:48 +0200

    Upgrade to TagSoup 1.2.1 (0.2.6).

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject clj-tagsoup "0.2.5"
+(defproject clj-tagsoup "0.2.6"
   :description "A HTML parser for Clojure."
-  :repositories {"jn" "http://download.java.net/maven/2"}
   :dependencies [[clojure "1.2.0"]
-                 [com.sun.phobos/tagsoup "1.2"]]
+                 [org.clojars.nathell/tagsoup "1.2.1"]]
   :dev-dependencies [[swank-clojure "1.2.0"]])
