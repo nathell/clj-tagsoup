aaf9add393d4ee0789f08fc7a54d60ed61625c59
Daniel Janus <dj@danieljanus.pl>
2012-04-24 12:58:25 +0100

    Drop swank-clojure from dev-dependencies. (If you use Emacs you should have swank-clojure installed as a Leiningen plugin anyway.)

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :description "A HTML parser for Clojure."
   :dependencies [[org.clojure/clojure "[1.2.0,)"]
                  [org.clojure/data.xml "0.0.3"]
-                 [org.clojars.nathell/tagsoup "1.2.1"]]
-  :dev-dependencies [[swank-clojure "1.2.0"]])
+                 [org.clojars.nathell/tagsoup "1.2.1"]])
