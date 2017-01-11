89b7c0381fe22003db26939ccdb2f06c305ad630
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2017-01-11 19:25:35 -0300

    Bump data.xml version so it'll work with clojure 1.9

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-tagsoup "0.3.0"
   :description "A HTML parser for Clojure."
   :dependencies [[org.clojure/clojure "[1.2.0,)"]
-                 [org.clojure/data.xml "0.0.3"]
+                 [org.clojure/data.xml "0.0.8"]
                  [org.clojars.nathell/tagsoup "1.2.1"]
                  [net.java.dev.stax-utils/stax-utils "20040917"]])
